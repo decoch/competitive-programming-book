54711eab4bdc8eda69f9f42667df8ed852bb0164
tominaga <tominaga.switch@gmail.com>
2018-04-19 23:35:57 +0900

    add correct

--- sort/bubble-sort.cpp
+++ sort/bubble-sort.cpp
@@ -34,10 +34,39 @@ void answer() {
   indicate(n, array);
 }
 
+int bubbleSort(int A[], int N) {
+  int sw = 0;
+  bool flag = 1;
+  for (int i = 0; flag; i++) {
+    flag = 0;
+    for(int j = N - 1; j >= i + 1; j--) {
+      if (A[j] < A[j - 1]) {
+        swap(A[j], A[j - 1]);
+        flag = 1;
+        sw++;
+      }
+    }
+  }
+  return sw;
+}
+
 void solve() {
+  int A[100], N, sw;
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> A[i];
+
+  sw = bubbleSort(A, N);
+
+  for (int i = 0; i < N; i++) {
+    if (i) cout << " ";
+    cout << A[i];
+  }
+
+  cout << endl;
+  cout << sw << endl;
 }
 
 int main() {
-  answer();
+  solve();
   return 0;
 }
